a057c716915ed32dcdc2deb7e863c3729ca6263a
hafizsidyck <hafizsidyck@gmail.com>
2024-05-27 16:11:59 +0200

    fini

--- JS/E3.js
+++ JS/E3.js
@@ -10,6 +10,13 @@ console.log(myString == 12);
 console.log(myString === 12);
 console.log(myString == null);
 console.log(myString == undefined);
+// max et min
+if (myNumber === "12") {
+    console.log("la condition est vraie");
+}
+if (myNumber > min && myNumber < max) {
+    console.log("myNumber est compris entre 10 et 20");
+}
 
 //if...else
 
@@ -26,6 +33,51 @@ if (myHour < 12) {
     heure = aprem;
 } else if (myHour > 19 && myHour <= 23) {
     heure = soir;
+}
+let myHour = prompt("Quelle heure est il (à 1 heure près) ?")
+if (typeof myHour == "number" && myHour > 12) {
+    console.log("oui");
+} else {
+    console.log("non");
+}
+// bonne maniére de programmé un switch
+
+const matin = "Matin";
+const aprem = "Après-midi";
+const soir = "Soir";
+let momentJournée = "";
+const heureLimiteMatin = 12;
+const heureLimiteAprem = 19;
+const heureLimiteSoir = 24;
+const casErreur = "cas non prévu";
+const boissonMatin = "café";
+const boissonAprem = "thé";
+const boissonSoir = "infusion";
+const boissonDefault = "eau";
+
+if (myHour < 12) {
+    momentJournée = matin;
+} else if (myHour < 19) {
+    momentJournée = "aprem";
+
+}else if (myHour < 24) {
+    momentJournée = "soir;"
+}
+console.log(momentJournée);
+
+switch (momentJournée) {
+    case matin:
+        console.log(boissonMatin);
+        break;
+    case aprem:
+        console.log(boissonAprem);
+        break;
+    case soir:
+        console.log(boissonSoir);
+        break;
+    default:
+        console.log(boissonDefault);
+        break;
 }
 
 // Switch...case
